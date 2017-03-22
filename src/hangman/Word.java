9db6f7a5152766a6d8656dcff0e637a97b397158
kowkon <sevindikberat@gmail.com>
2017-03-22 14:49:23 +0200

    fixed

---
+++ src/hangman/Word.java
@@ -0,0 +1,49 @@
+package hangman;
+
+public class Word {
+
+	private String content;
+	private String category;
+	private int length;
+
+	public Word(String content, String category) {
+		this.content = content;
+		this.category = category;
+		this.length = content.length();
+	}
+
+	public boolean contains(char ch) {
+		for (int i = 0; i < length; i++) {
+			if (content.charAt(i) == ch) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+}
